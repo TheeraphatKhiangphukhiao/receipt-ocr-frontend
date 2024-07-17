9e16fbd4ba273a94c940a6a5d3e40861796d5188
TheeraphatKhiangphukhiao <zerostar02yi@gmail.com>
2024-07-17 14:07:01 +0700

    Add periods to the end of sentences.

--- src/pages/edit page/EditPage.tsx
+++ src/pages/edit page/EditPage.tsx
@@ -3,7 +3,7 @@ import { useLocation } from "react-router-dom";
 import { ReceiptService } from "../../services/ReceiptService";
 import { useEffect, useState } from "react";
 import Spreadsheet, { Matrix } from "react-spreadsheet";
-import { Button } from "@mui/material";
+import { Button, Container } from "@mui/material";
 import "./editpage.css";
 import AddPhotoAlternateIcon from "@mui/icons-material/AddPhotoAlternate";
 import { useNavigate } from "react-router-dom";
@@ -12,23 +12,20 @@ import RemoveIcon from "@mui/icons-material/Remove";
 import SaveAsIcon from "@mui/icons-material/SaveAs";
 import React from "react";
 import CircularProgress from "@mui/material/CircularProgress";
-//import { useMediaQuery } from "react-responsive"; //สำหรับการทำให้เว็บ responsive ได้
 
 export default function EditPage() {
   const location = useLocation();
 
   const selectedImage = location.state.selectedImage; //ทำการรับรูปภาพที่ส่งมาจากหน้า CreatePage
 
-  const receiptService = new ReceiptService();
+  const receiptService = new ReceiptService(); //ทำการสร้าง instance ของ ReceiptService เพื่อเรียกใช้งานฟังก์ชั่น
 
   //create hook
-  const navigate = useNavigate(); // useNavigate hook
-
-  //const isTabletOrMobile = useMediaQuery({ query: "(max-width: 2000px)" });
-
+  const navigate = useNavigate(); //useNavigate hook
 
   const handleFile = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files && event.target.files[0];
+
     if (file) {
       setDataExcel([]);
       navigate("/edit", {
@@ -36,7 +33,6 @@ export default function EditPage() {
       });
     }
   };
-
 
   //ทำการประกาศ data excel โดยมีชนิดข้อมูล เป็น array 2D เเละข้างในเป็น json ที่มี key คือ value เเละค่าของมันเป็น string
   const [dataExcel, setDataExcel] = useState<{ value: string }[][]>([
@@ -48,15 +44,13 @@ export default function EditPage() {
     //[{ value: "A" }, { value: "B" }, { value: "C" }, { value: "D" }, { value: 'E' }],
   ]);
 
-
   //update ข้อมูลใหม่ทุกครั้งเมื่อมีการคีย์ข้อมูลลงใน spreadsheet
   function setnewdata(newData: Matrix<{ value: string }>) {
-    const cleanedData = newData.map(row =>
-      row.map(cell => ({ value: cell?.value || "" })) // Fallback to empty string if undefined
+    const cleanedData = newData.map(
+      (row) => row.map((cell) => ({ value: cell?.value || "" })) // Fallback to empty string if undefined
     );
     setDataExcel(cleanedData);
   }
-
 
   //เพิ่มแถวใหม่ใน Spreadsheet
   function addRow() {
@@ -65,13 +59,11 @@ export default function EditPage() {
     setDataExcel([...dataExcel, newRow]); //เพิ่มแถวใหม่ลงในข้อมูล
   }
 
-
   //เพิ่มคอลัมน์ใหม่ใน Spreadsheet
   function addColumn() {
     const newColumn = dataExcel.map((row) => [...row, { value: "" }]); // เพิ่มเซลล์ใหม่ลงในทุกแถว
     setDataExcel(newColumn); // ตั้งค่าข้อมูลใหม่
   }
-
 
   function deleteRow() {
     //รับข้อมูลที่เป็นอาร์เรย์ใหม่ที่ไม่รวมแถวสุดท้ายออกมา
@@ -79,22 +71,18 @@ export default function EditPage() {
     setDataExcel(newData);
   }
 
-
   function deleteColumn() {
     // เข้าถึง แถวทุกแถว และทำการสร้างอาร์เรย์ใหม่โดยไม่รวม column สุดท้าย
     const newData = dataExcel.map((row) => row.slice(0, -1));
     setDataExcel(newData);
   }
 
-
   //ฟังก์ชั่นสำหรับนำข้อมูลส่วนสำคัญของใบเสร็จไปเขียนเป็นไฟล์ csv เเล้วดาวน์โหลด
   async function save_data_as_csv() {
-
     console.log(dataExcel);
     const response = await receiptService.save_data_to_csv(dataExcel); //ทำการส่งข้อมูลไปเขียนไฟล์ เเล้วรับไฟล์ csv กลับมา
     console.log(response);
 
-    
     const bom = "\uFEFF";
     const blob = new Blob([bom + response], {
       type: "text/csv;charset=utf-8",
@@ -111,7 +99,6 @@ export default function EditPage() {
     document.body.removeChild(link);
   }
 
-
   //useEffect จะทำงานทุกครั้งเมื่อมีการ rerender
   useEffect(() => {
     console.log("Hello useEffect !!");
@@ -125,10 +112,10 @@ export default function EditPage() {
         ...response.result.map((item) => Object.keys(item).length)
       );
 
-
       //eslint-disable-next-line @typescript-eslint/no-explicit-any
       const newData = response.result.map((item: any) => {
         const rowData = [];
+
         for (let i = 1; i <= maxColumns; i++) {
           const key = `item${i}`;
           rowData.push({ value: item[key] || "" });
@@ -142,117 +129,21 @@ export default function EditPage() {
     loadDataAsync();
   }, [selectedImage]); //dependencies array กำหนดว่าฟังก์ชั่นนี้จะทำงานเมื่อ selectedImage มีการเปลี่ยนเเปลง
 
-
   return (
     <>
-      {/* <Container fixed> */}
-      <div className="editpage_body">
-        <div className="editpage_body_show1_buttoms_and_image">
-          <div className="editpage_body_show1_buttoms">
-            {/* ============================== ปุ่มเพิ่มรูแภาพใหม่ */}
-            <Button
-              sx={{
-                borderRadius: "30px",
-                border: "3px solid rgba(22, 49, 114, 1)",
-                color: "rgba(22, 49, 114, 1)",
-                paddingLeft: 2,
-                paddingRight: 4,
-                textTransform: "lowercase",
-                fontFamily: "Kanit",
-                fontSize: "18px",
-              }}
-              variant="outlined"
-              component="label"
-            >
-              <AddPhotoAlternateIcon sx={{ width: 30, height: 30 }} />
-              New
-              <input
-                type="file"
-                accept="image/*"
-                hidden
-                onChange={handleFile}
-              />
-            </Button>
-          </div>
+      <Container fixed sx={{ mt: 5 }}>
+        <div className="inline-div">
+          <img className="responsive-image" src={URL.createObjectURL(selectedImage)} alt="" />
 
-          <img
-            className="editpage_body_show1_image"
-            src={URL.createObjectURL(selectedImage)}
-            alt="Selected Image"
-          />
-        </div>
-
-        <div className="editpage_body_show2_buttoms_and_spreadsheet">
-          <div className="editpage_body_show2_buttoms">
-            {/*==================================================================== add Row and delete Row */}
-            <div className="editpage_body_show2_buttoms_addRow_save_dleteRow">
-              {/* =================== add Row */}
-              <button
-                className="editpage_body_show2_buttoms_addRow_addColumn"
-                onClick={addRow}
-              >
-                <AddIcon sx={{ fontSize: "20px" }} />
-                <span>Row</span>
-              </button>
-
-              {/* =================== delete Row */}
-              <button
-                className="editpage_body_show2_buttoms_deleteRow_deleteColumn"
-                onClick={deleteRow}
-              >
-                <RemoveIcon sx={{ fontSize: "20px" }} />
-                <span>Row</span>
-              </button>
-            </div>
-
-            {/*================================================================== save */}
-            <button
-              className="editpage_body_show2_buttoms_save"
-              onClick={save_data_as_csv}
-            >
-              <SaveAsIcon sx={{ fontSize: "20px" }} />
-              <span>Save</span>
-            </button>
-
-            {/*=================================================================== delete Column and delete Column */}
-            <div className="editpage_body_show2_buttoms_addColumn_save_dleteColumn">
-              {/* =================== add Column */}
-              <button
-                className="editpage_body_show2_buttoms_addRow_addColumn"
-                onClick={addColumn}
-              >
-                <AddIcon sx={{ fontSize: "20px" }} />
-                <span>Column</span>
-              </button>
-
-              {/* =================== delete Column */}
-              <button
-                className="editpage_body_show2_buttoms_deleteRow_deleteColumn"
-                onClick={deleteColumn}
-              >
-                <RemoveIcon sx={{ fontSize: "20px" }} />
-                <span>Column</span>
-              </button>
-            </div>
-          </div>
-
-          <div className="editpage_body_show2_spreadsheet">
+          <div className="custom-spreadsheet-one">
             {dataExcel.length > 0 ? (
-              <Spreadsheet
-                className="editpage_body_show2_custom_spreadsheet"
-                data={dataExcel}
-                onChange={setnewdata}
-                onKeyDown={(event) => console.log(event.key)}
-              />
+              <Spreadsheet className="custom-spreadsheet-two" data={dataExcel} />
             ) : (
-              <div className="editpage_body_show2_spreadsheet_loading">
-                <CircularProgress />
-              </div>
+              <div></div>
             )}
           </div>
         </div>
-      </div>
-      {/* </Container> */}
+      </Container>
     </>
   );
 }
